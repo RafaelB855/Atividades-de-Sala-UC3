a9ad87acbc90ea9657d5b36d88a1839f36fd5173
RafaelBarbosa85 <rafael818b@gmail.com>
2023-06-25 15:11:38 -0300

    25 06

--- Trabalho Final(em construção)/teste.py
+++ Trabalho Final(em construção)/teste.py
@@ -5,35 +5,41 @@ conexaoBanco = Conexao("ClickMed","localhost","5432","postgres","postgres")
 
 
 
-lista = [1,2,3,4,1,6,4,4,4,2,6,4,6,1,4,1,]
+lista = [1,2,3,4,1,6,4,4,4,2,6,4,6,1,4,1]
+
+# lista2 = lista
+# print (lista.set)
 
 resultado = {}
 
 for i in lista:
     if i not in resultado.keys():
         resultado[i] = lista.count(i)
+    # for i in resultado.values():
+    #     if  i%2 == 0:
+    #     resultado.popitem(i)
 
 print(resultado)
 
 m = max(resultado.values())
 y = next(k for k, v in resultado.items() if v == m)
 
-print(y)
+# print(y)
 
-listafinal= []
-listasemifinal = []
+# listafinal= []
+# listasemifinal = []
 
-listadoença=["dasdas1","dasdsa2",3]
+# listadoença=["dasdas1","dasdsa2",3]
 
-listasintomas=[156,"dsadsad3"]
+# listasintomas=[156,"dsadsad3"]
 
-coisas = (listasintomas[1], listadoença[1] ,listasintomas[0], listadoença[2])
+# coisas = (listasintomas[1], listadoença[1] ,listasintomas[0], listadoença[2])
 
-listafinal.append(coisas)
+# listafinal.append(coisas)
 
-print(listafinal)
+# print(listafinal)
 
-rafa= "rafa"
+# rafa= "rafa"
 
-rafa2 = rafa.upper()
-print(rafa,rafa2)
+# rafa2 = rafa.upper()
+# print(rafa,rafa2)
